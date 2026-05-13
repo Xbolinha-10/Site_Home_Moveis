d31e854e21cdb4186e0ad9dc3c6230302be1eb93
Xbolinha-10 <brenohenriqueshimada@gmail.com>
2026-05-13 12:32:59 -0300

    Atualizei o server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // Configurações Iniciais
 const cors = require('cors');
